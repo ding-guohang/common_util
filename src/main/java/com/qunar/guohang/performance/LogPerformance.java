ed0454a09f516fdeed6a12a00a1eac51aebf62b2
guohang.ding <guohang.ding@qunar.com>
2016-10-02 11:50:22 +0800

    init annotation for logPerformance

---
+++ src/main/java/com/qunar/guohang/performance/LogPerformance.java
@@ -0,0 +1,27 @@
+package com.qunar.guohang.performance;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 关注性能
+ * 主要关注方法耗时
+ * <p>
+ * 可以配置流程块，监控一小块整个耗时
+ *
+ * @author guohang.ding on 16-10-2
+ */
+@Inherited
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogPerformance {
+
+    /** 流程块， 默认不关注流程块 */
+    String flow() default "";
+
+    /** 该方法是否是流程块最后一个方法 */
+    boolean finish() default false;
+}
