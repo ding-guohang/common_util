2d43d4be00fe50dea73ace906b9fac6c42c91bda
guohang.ding <guohang.ding@qunar.com>
2016-10-08 08:48:28 +0800

    add md

--- src/main/java/com/qunar/guohang/performance/LogPerformance.java
+++ src/main/java/com/qunar/guohang/performance/LogPerformance.java
@@ -18,6 +18,9 @@ import java.lang.annotation.Target;
  * 允许一次请求中出现多个流程块重叠，但是某个方法只能作为某一个流程块的成员 -> 所以这个设定蛮鸡肋的
  * 但是想象不出有什么场景需要多个流程块重叠
  *
+ * updated by guohang.ding on 16-10-6
+ * 通过实现MethodFilter进行监控扩展，使用exclusion属性进行filter的屏蔽
+ *
  * @author guohang.ding on 16-10-2
  */
 @Inherited
