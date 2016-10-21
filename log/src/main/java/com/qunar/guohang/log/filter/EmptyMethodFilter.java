d73cda4244ab6d1203900b513d04c4f63fd9e969
guohang.ding <guohang.ding@qunar.com>
2016-10-21 17:40:40 +0800

    fix

---
+++ log/src/main/java/com/qunar/guohang/log/filter/EmptyMethodFilter.java
@@ -0,0 +1,26 @@
+package com.qunar.guohang.log.filter;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author guohang.ding on 16-10-21
+ */
+@Service
+public class EmptyMethodFilter extends ComparableMethodFilter {
+
+    @Override
+    public void preHandle(ProceedingJoinPoint pjp) {
+
+    }
+
+    @Override
+    public void postHandle(ProceedingJoinPoint pjp) {
+
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
