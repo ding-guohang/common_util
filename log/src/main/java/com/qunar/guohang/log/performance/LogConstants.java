072c4fd7bba1fda03879f8767a0ff650e250df14
guohang.ding <guohang.ding@qunar.com>
2016-10-24 09:08:22 +0800

    fix

--- log/src/main/java/com/qunar/guohang/log/performance/LogConstants.java
+++ log/src/main/java/com/qunar/guohang/log/performance/LogConstants.java
@@ -1,12 +1,13 @@
 package com.qunar.guohang.log.performance;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.Maps;
 import com.qunar.guohang.log.strategies.ParamStrategy;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Constants for log and performance
@@ -22,10 +23,9 @@ public class LogConstants {
         }
     };
 
-    private static ConcurrentHashMap<Class<? extends ParamStrategy>, ParamStrategy> strategies = new ConcurrentHashMap<Class<? extends ParamStrategy>, ParamStrategy>();
+    private static ConcurrentMap<Class<? extends ParamStrategy>, ParamStrategy> strategies = Maps.newConcurrentMap();
 
-    public static final String SEPARATOR = "_";
-    public static final Joiner JOINER = Joiner.on(SEPARATOR);
+    public static final Joiner JOINER = Joiner.on("_");
     public static final String COST_FORMAT = "{} cost {}";
 
     public static void setFlow(String name, long cost) {
@@ -51,6 +51,6 @@ public class LogConstants {
     }
 
     public static List<ParamStrategy> buildStrategies() {
-        return Collections.unmodifiableList(new ArrayList<ParamStrategy>(strategies.values()));
+        return Collections.unmodifiableList(new ArrayList<>(strategies.values()));
     }
 }
