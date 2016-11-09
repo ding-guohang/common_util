c8a0e43e41ebfc939496752cc72597f6f3d55e02
guohang.ding <guohang.ding@qunar.com>
2016-11-09 16:55:27 +0800

    add docx

--- log/src/main/java/com/qunar/flight/log/strategies/StrategiesLoader.java
+++ log/src/main/java/com/qunar/flight/log/strategies/StrategiesLoader.java
@@ -5,12 +5,18 @@ import java.util.ServiceLoader;
 /**
  * Strategies Loader
  * Use ServiceLoader now
- * Is extensionLoader better?
+ * Is the way of ExtensionLoader better?
  *
  * @author guohang.ding on 16-11-9
  */
 public class StrategiesLoader {
 
+    /**
+     * 基于Iterable、LazyIterator实现懒加载
+     * 加载就是读文件、实例化
+     * 基于LinkedHashMap做缓存
+     * 非线程安全
+     */
     private static final ServiceLoader<ParamStrategy> loader = ServiceLoader.load(ParamStrategy.class);
 
     public static Iterable<ParamStrategy> getStrategies() {
